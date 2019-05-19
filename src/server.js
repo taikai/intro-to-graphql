68259ab0c093b91c6f971e3cb40bec59b807783d
lgoncalves <exnlesi@gmail.com>
2019-05-19 23:16:46 +0100

    setup starting code

--- src/server.js
+++ src/server.js
@@ -1,35 +1,24 @@
 import { ApolloServer } from 'apollo-server'
-import { loadTypeSchema } from './utils/schema'
-import { authenticate } from './utils/auth'
-import { merge } from 'lodash'
-import config from './config'
-import { connect } from './db'
-import product from './types/product/product.resolvers'
-import coupon from './types/coupon/coupon.resolvers'
-import user from './types/user/user.resolvers'
-
-const types = ['product', 'coupon', 'user']
 
 export const start = async () => {
+  // Define Schema using SDL ( Schema Definition Language )
   const rootSchema = `
+    type Query {
+      hello: String
+    }
     schema {
       query: Query
-      mutation: Mutation
     }
   `
-  const schemaTypes = await Promise.all(types.map(loadTypeSchema))
+
+  const resolvers = {}
 
   const server = new ApolloServer({
-    typeDefs: [rootSchema, ...schemaTypes],
-    resolvers: merge({}, product, coupon, user),
-    async context({ req }) {
-      const user = await authenticate(req)
-      return { user }
-    }
+    typeDefs: [rootSchema],
+    resolvers: resolvers
   })
 
-  await connect(config.dbUrl)
-  const { url } = await server.listen({ port: config.port })
+  const { url } = await server.listen({ port: 3000 })
 
   console.log(`GQL server ready at ${url}`)
 }
