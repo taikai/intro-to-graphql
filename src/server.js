2383100e780b2fc59e6cc391ca6d72689be0ec75
lgoncalves <exnlesi@gmail.com>
2019-05-21 21:12:02 +0100

    demo live presentation

--- src/server.js
+++ src/server.js
@@ -1,21 +1,106 @@
 import { ApolloServer } from 'apollo-server'
 
+let companyList = [
+  {
+    id: '1',
+    name: 'Taikai',
+    tags: ['Innovation', 'Challenges'],
+    yearFounded: 2018,
+    employees: [
+      {
+        id: '1',
+        name: 'Mário Alves'
+      },
+      {
+        id: '2',
+        name: 'Helder Vasconcelos'
+      }
+    ]
+  },
+  {
+    id: '2',
+    name: 'AppyBook',
+    tags: ['Health'],
+    yearFounded: 2018,
+    employees: [
+      {
+        id: 't1',
+        name: 'Ricardo'
+      }
+    ]
+  }
+]
+
 export const start = async () => {
   // Define Schema using SDL ( Schema Definition Language )
   const rootSchema = `
-    type Query {
-      hello: String
+    type Company {
+      id: ID!
+      name: String
+      tags: [String]
+      yearFounded: Int!
+      employees: [Person]
+    }  
+
+    type Person {
+      id: ID!
+      name: String
+      company: Company
     }
+
+    input CompanyInput {
+      name: String!
+      tags: [String]
+      yearFounded: Int!
+    }
+
+    type Query {
+      companies: [Company]
+    }
+
+    type Mutation {
+      addCompany(data: CompanyInput): Company! 
+    }
+
     schema {
       query: Query
+      mutation: Mutation
     }
   `
 
-  const resolvers = {}
+  const resolvers = {
+    Query: {
+      companies: (_, args, context, info) => {
+        if (!context.user.isLoggedIn) {
+          return []
+        } else {
+          return companyList
+        }
+      }
+    },
+    Mutation: {
+      addCompany: (_, args) => {
+        const { data } = args
+        const newCompany = {
+          id: Date.now(),
+          name: data.name,
+          tags: data.tags,
+          yearFounded: data.yearFounded
+        }
+        companyList.push(newCompany)
+
+        return newCompany
+      }
+    }
+  }
 
   const server = new ApolloServer({
     typeDefs: [rootSchema],
-    resolvers: resolvers
+    resolvers: resolvers,
+    context: req => {
+      // example auth: const user = checkAuth(req)
+      return { user: { isLoggedIn: true } }
+    }
   })
 
   const { url } = await server.listen({ port: 3000 })
